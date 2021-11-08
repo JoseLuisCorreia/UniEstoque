0c4271ba73f117bbc8e4081a3b011a0a55f970bb
JoseLuisCorreia <82620439+JoseLuisCorreia@users.noreply.github.com>
2021-11-08 19:17:29 -0300

    Teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PaginaComponent } from './pagina/pagina.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'pagina', pathMatch: 'full' },
+  {path: '', redirectTo: 'https://unruffled-jo-luis-42a4fse.netlify.app/', pathMatch: 'full' },
   {path: 'menu' , component: MenuComponent},
   {path:'sobre' ,component: SobreComponent},
   {path: 'pagina' ,component: PaginaComponent },
